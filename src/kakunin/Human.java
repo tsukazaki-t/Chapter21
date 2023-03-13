33ebb30a037ab3086982b4ace69c6549d4ff6ca6
TomomiTsukazaki <122886012+tsukazaki-t@users.noreply.github.com>
2023-03-13 20:32:57 +0900

    課題提出

---
+++ src/kakunin/Human.java
@@ -0,0 +1,11 @@
+package kakunin;
+
+public class Human {
+	
+	public  class HumanLegs{
+		public void kick(){
+			System.out.println("蹴る");
+		}
+	}
+
+}
---
+++ src/kakunin/Main.java
@@ -0,0 +1,14 @@
+package kakunin;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		Human human = new Human();
+		Human.HumanLegs humanLegs = human.new HumanLegs();
+		humanLegs.kick();
+
+	}
+
+}
